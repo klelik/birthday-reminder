94d9134df61671e68352f17a08ab83a1f6bb36fa
klelik <klelika1998@gmail.com>
2022-12-05 16:50:28 +0000

    Updates

---
+++ precache-manifest.9f2b259ef63500a35c5d6966435cc84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc59a20b0000c1a8aeb46b8dd46df82",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "3d47f4e190a65910814a",
+    "url": "/birthday-reminder/static/css/main.9691e530.chunk.css"
+  },
+  {
+    "revision": "cf9fd6d41456e2040972",
+    "url": "/birthday-reminder/static/js/2.297f8484.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.297f8484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d47f4e190a65910814a",
+    "url": "/birthday-reminder/static/js/main.42df6a88.chunk.js"
+  },
+  {
+    "revision": "a1ff553ac3d675eebff3",
+    "url": "/birthday-reminder/static/js/runtime-main.ed31b697.js"
+  }
+]);
